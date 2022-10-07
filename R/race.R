8ae93731a5292eae781e3b731a7e645985639776
nathaniel-morgan <nathaniel.morgan1999@gmail.com>
2022-10-07 15:23:17 -0500

    Documenting the Race Function

--- R/race.R
+++ R/race.R
@@ -1,3 +1,14 @@
+#' Race two functions
+#'
+#' @param a A process you want to compare the run time with process b
+#' @param b The process to compare the run time against process a
+#'
+#' @return a length 1 double of the difference in seconds process a took compared to process b.
+#' @export
+#'
+#' @examples
+#' race(rnorm(1e5),rnorm(1e7))
+#' race(rnorm(1e7),rnorm(1e5))
 race <- function(a,b) {
   system.time(a)[[3]]-system.time(b)[[3]]
 }
